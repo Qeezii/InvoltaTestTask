04252e50139a5d90ed39fb35e0b8d88ddda1118a
Alexey Poletaev <av.poletaev21@gmail.com>
2023-06-15 08:21:42 +0300

    Removed unnecessary code

---
+++ InvoltaTestTask/Application/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  InvoltaTestTask
+//
+//  Created by Alexey Poletaev on 15.06.2023.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+        return true
+    }
+}
+
